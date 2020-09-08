df075216568680470d844576acfc1d7fd20b6b50
Adrian Stepien <adrian.stepien.fsw@gmail.com>
2020-09-08 19:39:02 +0200

    Added map and adding location in locationComponent.js

--- src/screens/LocationComponent.js
+++ src/screens/LocationComponent.js
@@ -1,73 +1,133 @@
-import React, { useState }  from 'react';
-import {
-  Button,
-  View,
-} from 'react-native';
-import {
-  List,
-  Text,
-} from 'react-native-paper';
+import React, { useState, useEffect }  from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import * as Location from 'expo-location';
+import SimpleMap from './SimpleMap';
+import {Button, Paragraph, Dialog, Portal, HelperText, Subheading, TextInput} from 'react-native-paper';
 
-const REJECTION_MESSAGE = 'Permission to access location was denied';
+const CONTROL_STATE = {NONE: 0, ADD: 1, REMOVE: 2, ADDING_FORM: 3};
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+  },
+  container: {
+    width: '100%', 
+    flexDirection: 'column'
+  },
+  containerButtons: {
+    width: '100%', 
+    flexDirection: 'row',
+    height: 50
+  },
+  containerButton: {
+    width: '46%', 
+    margin: '2%'
+  }
+});
+
+
+function drawButtons(state, setControlState) {
+  if (state == CONTROL_STATE.NONE) {
+    return(   
+      <View style={styles.containerButtons}>
+        <View style={styles.containerButton}>
+          <Button
+            mode="contained"
+            onPress={() => setControlState(CONTROL_STATE.ADD)}
+            style={styles.button}
+          >
+            Add location
+          </Button>
+        </View>
+        <View style={styles.containerButton}>
+          <Button
+            mode="contained"
+            onPress={() => setControlState(CONTROL_STATE.REMOVE)}
+            style={styles.button}
+          >
+            Remove Location
+          </Button>  
+        </View>
+      </View>
+    )
+
+  }else if (state == CONTROL_STATE.ADD || state == CONTROL_STATE.ADDING_FORM){
+    return(    
+      <View style={styles.containerButtons}>
+        <Text style={{margin:15}}>Przytrzymaj palec na lokalizacji aby ją dodać!</Text>
+      </View>
+    )
+
+  }else if (state == CONTROL_STATE.REMOVE){
+    return(    
+      <View style={styles.containerButtons}>
+        <Text style={{margin:15}}>Przytrzymaj palec na lokalizacji aby ją usunąć.</Text>
+      </View>
+    )
+  }
+}
+
+function confirmAdding(activityName, range, cords){
+}
+
 
 function LocationComponent() {
+  const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [locations, setLocations] = useState([]);
+  const [controlState, setControlState] = useState(CONTROL_STATE.NONE);
+  const [activityName, setActivityName] = useState(null);
+  const [range, setRange] = useState(20);
+  const [cords, setCords] = useState(null);
+  
 
-  const listItems = locations.map((singleLocation, index) =>  {
-    const { latitude, longitude, timestamp } = singleLocation;
-    return (
-      <List.Item
-        key={timestamp}
-        title={'LOCATION #' + (index + 1)}
-        description={() => (
-          <View>
-            <Text>Latitude: {latitude}</Text>
-            <Text>Longitude: {longitude}</Text>
-            <Text>Timestamp: {timestamp}</Text>
-          </View>
-        )}
-      />
-    );
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+      }
+      let location = await Location.getCurrentPositionAsync();
+      setLocation(location);
+    })();
   });
 
-  const requestLocation = async () => {
-    Location.requestPermissionsAsync().then(async (responseData) => {
-      let { status } = responseData;
-
-      if (status === 'granted') {
-        let location = await Location.getCurrentPositionAsync();
-
-        let { coords, timestamp } = location;
-        let { longitude, latitude } = coords;
-        setLocations([...locations, {
-          latitude,
-          longitude,
-          timestamp,
-        }]);
-
-        setErrorMsg(false);
-      } else {
-        setErrorMsg(REJECTION_MESSAGE);
-      }
-    }).catch(() => {
-      setErrorMsg(REJECTION_MESSAGE);
-    });
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    let { coords, timestamp } = location;
+    let { longitude, latitude } = coords;
+    text = `Longitude: ${longitude} | Latitude: ${latitude} | Timestamp: ${timestamp}`;
   }
 
   return (
-    <View>
-      <Button
-        title="Request location"
-        onPress={requestLocation}
-      />
-      {errorMsg ? (
-        <Text>{errorMsg}</Text>
-      ):(
-        listItems
-      )}
-    </View>
+      <View style={styles.container}>
+        {drawButtons(controlState, setControlState)}
+        <Portal>
+          <Dialog visible={controlState == CONTROL_STATE.ADDING_FORM} onDismiss={() => setControlState(CONTROL_STATE.NONE)}>
+            <Dialog.Title>Adding location</Dialog.Title>
+            <Dialog.Content>
+              <Paragraph></Paragraph>
+              <TextInput
+                keyboardType = 'numeric'
+                label="Range [meters]"
+                mode="outlined"
+                onChangeText={(text) => setActivityName(text)}
+              />
+              <TextInput
+                label="Activity name"
+                mode="outlined"
+                onChangeText={(text) => setRange(text)}
+              />
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={() => {setControlState(CONTROL_STATE.NONE); confirmAdding(activityName, range, cords)}}>Done</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+        <SimpleMap onLongPress={ (cords) => {setCords(cords); if(controlState==CONTROL_STATE.REMOVE) setControlState(CONTROL_STATE.NONE); else if(controlState==CONTROL_STATE.ADD) setControlState(CONTROL_STATE.ADDING_FORM)} }></SimpleMap>
+        <Text>{text}</Text>
+      </View>
   );
 }
 
---
+++ src/screens/SimpleMap.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default function SimpleMap(props){
+  return (
+    <MapView style={styles.mapStyle}    
+      initialRegion={{
+        latitude: 52.4144,
+        longitude: 16.9211,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,}}
+        showsUserLocation = {true}
+        onLongPress = {(e) => {props.onLongPress(e.nativeEvent.coordinate)}}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height*0.85,
+  },
+});
